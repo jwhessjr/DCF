0b6e910b4caa5f7e871e41b07c0c7d08b7341c39
jwhessjr <jhess2@gmail.com>
2021-10-07 20:55:07 -0400

    first code to read csv

--- Read_CSV.py
+++ Read_CSV.py
@@ -1,2 +1,6 @@
-# test rsa key set up to github
-# second test
+import csv
+
+with open('FLDCF20211006.csv', newline=") as dcfinputs:
+    dcfvalues = csv.reader(dcfinputs, delimeter='', quotecharacter='|')
+    for row in dcfvalues:
+        print(','.join(row))
