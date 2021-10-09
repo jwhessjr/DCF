fd1b06cddcc9aad0ded3398d4f87b1af6327b623
jwhessjr <jhess2@gmail.com>
2021-10-09 06:46:20 -0400

    Commit module to create dict

--- Read_CSV_Dict.py
+++ Read_CSV_Dict.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-flDict = pd.read_csv(FLDCF20211006.csv, index_col=0,
-                     header=None, squeeze=True), to_dict()
+flDict = pd.read_csv('FLDCF20211006.csv', index_col=0,
+                     header=None, squeeze=True).to_dict()
 
-print(flDict)
+for key in flDict:
+    print(key, flDict[key])
