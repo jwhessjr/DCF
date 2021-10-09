bad5c74f280c8540c1d787112c421bd6faad0a73
jwhessjr <jhess2@gmail.com>
2021-10-09 06:57:24 -0400

    Change print of keys and values

--- Read_CSV_Dict.py
+++ Read_CSV_Dict.py
@@ -3,5 +3,5 @@ import pandas as pd
 flDict = pd.read_csv('FLDCF20211006.csv', index_col=0,
                      header=None, squeeze=True).to_dict()
 
-for key in flDict:
-    print(key, flDict[key])
+for key, value in flDict.items():
+    print(key, value)
