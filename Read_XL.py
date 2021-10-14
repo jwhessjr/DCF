3e82220354ce2df07eecf340e35a98c0c6af0a1f
jwhessjr <jhess2@gmail.com>
2021-10-14 16:28:32 -0400

    Code to read and use  data items in a pandas file

--- Read_XL.py
+++ Read_XL.py
@@ -3,6 +3,7 @@ import pandas as pd
 file = "FLDCF20211006.xlsx"
 
 inputs = pd.read_excel(file, sheet_name=0, index_col=0)
+
 bal_sheet = pd.read_excel(file, sheet_name=1, index_col=0)
 inc_stmnt = pd.read_excel(file, sheet_name=2, index_col=0)
 cash_flow = pd.read_excel(file, sheet_name=3, index_col=0)
@@ -11,5 +12,17 @@ cash_flow = pd.read_excel(file, sheet_name=3, index_col=0)
 # print(bal_sheet)
 # print(inc_stmnt)
 # print(cash_flow)
+
 dcf_inputs = pd.concat([inputs, bal_sheet, inc_stmnt, cash_flow])
-print(dcf_inputs)
+# print(dcf_inputs)
+# print(dcf_inputs.loc["Book Value of Equity", "Current Year"])
+book_value = (dcf_inputs.loc["Book Value of Equity", "Current Year"] +
+              dcf_inputs.loc["Book Value of Equity", "Prior Year"]) / 2
+
+print(book_value)
+
+# print(dcf_inputs.loc["Current Year"])
+#print(dcf_inputs.loc["Book Value of Equity", "Current Year"])
+#
+
+# print(dcf_inputs.dtypes)
